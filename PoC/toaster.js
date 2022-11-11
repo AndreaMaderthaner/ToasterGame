c128ae0c48cd5a5b0e229394cf23f849a76f12af
Axel Goris <axelgoris99@gmail.com>
2022-11-11 17:59:09 +0100

    Small fixes : Changed the frequencies to be notes. Changed the number of note to be a proportion of the max value. Added some timeout between notes being played. Change the duration so that it is actually filling the buffer with the correct amount of notes :)

--- PoC/toaster.js
+++ PoC/toaster.js
@@ -1,18 +1,28 @@
 let audioContext = new AudioContext();
-let frequencies = [110, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
+// let frequencies = [110, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
+let frequencies1 = [261.6256, 246.9417, 220.0, 195.9977];
+let frequencies2 = [261.6256, 293.6648, 329.6276, 349.2282];
+
 let playerOne = {};
 let playerTwo = {};
 
-function assignFrequencies(player) {
+let readyToPlay = true;
+
+let timeForANote = 1;
+
+let maxConsumption = 1000;
+
+function assignFrequencies(player, frequencies) {
   let selectedFrequencies = [...frequencies];
+  //changed so that the number of notes is a percent of the player consumption
   return shuffleArray(selectedFrequencies).slice(
     0,
-    Math.round(frequencies.length - player.consumption / 100 + 1)
+    Math.round((frequencies.length * player.consumption) / maxConsumption + 1)
   );
 }
 
 function getEnergyConsumption() {
-  return Math.round(Math.random() * (1000 - 100) + 100);
+  return Math.round(Math.random() * (maxConsumption - 100) + 100);
 }
 
 function shuffleArray(array) {
@@ -24,14 +34,20 @@ function shuffleArray(array) {
 }
 
 function playFrequency(frequency) {
+  readyToPlay = false;
+
   // create 2 second worth of audio buffer, with single channels and sampling rate of your device.
+
+  //create 10 seconds worth of audio buffer
   let sampleRate = audioContext.sampleRate;
-  let duration = 2 * sampleRate;
+  let duration = timeForANote * sampleRate;
   let numChannels = 1;
   let buffer = audioContext.createBuffer(numChannels, duration, sampleRate);
+  console.log(buffer);
   // fill the channel with the desired frequency's data
   let channelData = buffer.getChannelData(0);
-  for (let i = 0; i < sampleRate; i++) {
+  // change sampleRate to use duration instead becaue otherwise you don't fill the value in channelData buffer meaning you don't have the correct duration
+  for (let i = 0; i < duration; i++) {
     channelData[i] = Math.sin((2 * Math.PI * frequency * i) / sampleRate);
   }
 
@@ -42,14 +58,18 @@ function playFrequency(frequency) {
 
   // finally start to play
   source.start(0);
+  // just to give some time to play the sound
+  setTimeout(() => {
+    readyToPlay = true;
+  }, 500);
 }
 
 $(document).ready(function () {
   $("#start").click(function () {
     playerOne.consumption = getEnergyConsumption();
     playerTwo.consumption = getEnergyConsumption();
-    playerOne.frequencies = assignFrequencies(playerOne);
-    playerTwo.frequencies = assignFrequencies(playerTwo);
+    playerOne.frequencies = assignFrequencies(playerOne, frequencies1);
+    playerTwo.frequencies = assignFrequencies(playerTwo, frequencies2);
     playerOne.index = playerTwo.index = 0;
 
     $("#energyOne").text(playerOne.consumption);
@@ -61,6 +81,7 @@ $(document).ready(function () {
   });
 
   $(window).bind("keypress", function (e) {
+    if (!readyToPlay) return;
     if (e.keyCode == 97) {
       playFrequency(
         playerOne.frequencies[playerOne.index++ % playerOne.frequencies.length]
